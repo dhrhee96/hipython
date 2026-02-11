92c64ec0b60d9862f794514d27ca6d949d253727
dhrhee96 <dhrhee96@gmail.com>
2026-02-11 17:40:59 +0900

    저장

--- streamlit_exam/app.py
+++ streamlit_exam/app.py
@@ -2,4 +2,6 @@ import streamlit as st
 st.title("Hello, Streamlit!")
 st.write("This is a simple Streamlit application.")
 st.write("You can add more components and interactivity as needed.") 
+st.write("Enjoy building your app!")
+st.write("Here's a button for you to click:")
 st.button("Click Me!")
