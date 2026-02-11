559d851ddaea1d4f900fef9cb40a688a2e4cb9b3
dhrhee96 <dhrhee96@gmail.com>
2026-02-11 17:26:47 +0900

    새로운파일

---
+++ streamlit_exam/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Hello, Streamlit!")
+st.write("This is a simple Streamlit application.")
+st.write("You can add more components and interactivity as needed.") 
+st.button("Click Me!")
